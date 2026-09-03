53b55911a56f7590808e350a1ba52d26df195808
soory <soory@SOORYA>
2026-09-03 12:20:32 +0530

    version 1 - inital commit

--- CaseTrackerMobile/MainPage.xaml.cs
+++ CaseTrackerMobile/MainPage.xaml.cs
@@ -9,34 +9,6 @@ namespace CaseTrackerMobile
         public MainPage()
         {
             InitializeComponent();
-
-            // Initialize with Dashboard view
-            ContentArea.Content = new DashboardView();
-        }
-
-        private void OnDashboardClicked(object sender, EventArgs e)
-        {
-            ContentArea.Content = new DashboardView();
-        }
-
-        private void OnCasesClicked(object sender, EventArgs e)
-        {
-            ContentArea.Content = new CasesView();
-        }
-
-        private void OnClientsClicked(object sender, EventArgs e)
-        {
-            ContentArea.Content = new ClientsView();
-        }
-
-        private void OnDocumentsClicked(object sender, EventArgs e)
-        {
-            ContentArea.Content = new DocumentsView();
-        }
-
-        private void OnProfileClicked(object sender, EventArgs e)
-        {
-            ContentArea.Content = new ProfileView();
         }
     }
 }
